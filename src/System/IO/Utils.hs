df7fb9fec85fa85d22be361e42c1643ee14f9e87
Daniel Lyons <fusion@storytotell.org>
2011-02-01 23:38:02 -0700

    moving getPassword and withEcho into System.IO.Utils from Database, where they didn't belong.

---
+++ src/System/IO/Utils.hs
@@ -0,0 +1,17 @@
+module Utils (getPassword, withEcho) where
+
+import Control.Exception
+import System.IO
+
+getPassword :: IO String
+getPassword = do
+  putStr "Password: "
+  hFlush stdout
+  pass <- withEcho False getLine
+  putChar '\n'
+  return pass
+
+withEcho :: Bool -> IO a -> IO a
+withEcho echo action = do
+  old <- hGetEcho stdin
+  bracket_ (hSetEcho stdin echo) (hSetEcho stdin old) action
